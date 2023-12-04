e8999b02d568e4212f10a98e248719fbf34d8263
opilane <opilane@HP-CZC2349HTD>
2023-12-04 11:57:03 +0200

    Add project files.

---
+++ Maakonnad2/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Maakonnad2;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Maakonnad2
+{
+    public partial class MainPage : ContentPage
+    {
+        string filename;
+        string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        protected internal ObservableCollection<Maakond> Maakonnads { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            Maakonnads = new ObservableCollection<Maakond>
+            {
+                new Maakond {Nimetus="Harjumaa",Pealinn="Tallinn", Inimeste_arv=52000},
+                new Maakond {Nimetus="Tartumaa",Pealinn="Tartu", Inimeste_arv=30000},
+                new Maakond {Nimetus="Ida-Virumaa",Pealinn="Kohtla-Järve", Inimeste_arv=10000},
+            };
+            list.BindingContext = Maakonnads;
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Maakonnad(null));
+        }
+
+        private async void list_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            Maakond selected = e.SelectedItem as Maakond;
+            if (selected != null)
+            {
+                list.SelectedItem = null;
+                await Navigation.PushAsync(new Maakonnad(selected));
+            }
+
+
+        }
+        protected internal void AddMaakond(Maakond maakond)
+        {
+            Maakonnads.Add(maakond);
+        }
+    }
+}
+
+
